a2476cba7a777bff881f00929e28abf07e6b2759
ricardo <ricardoallcca04@gmail.com>
2015-11-22 22:58:27 -0500

    new game

--- holamichi/MainActivity.java
+++ holamichi/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
@@ -17,9 +18,8 @@ public class MainActivity extends Activity implements OnClickListener {
 	
 	boolean turn =true; //true =X & false=O
 	int turn_count =0;
-	Button[]bArray=null;
-	Button a1,a2,a3,b1,b2,b3,c1,c2,c3;
-	
+	Button[]bArray;
+	Button a1,a2,a3,b1,b2,b3,c1,c2,c3,NewGame;
 	
 	
 	//Ahora tenemos nuestro diseño listo 
@@ -39,12 +39,24 @@ public class MainActivity extends Activity implements OnClickListener {
         a3=(Button)findViewById(R.id.A3);
         b3=(Button)findViewById(R.id.B3);
         c3=(Button)findViewById(R.id.C3);
+        NewGame=(Button)findViewById(R.id.NewGame);
+        
         bArray=new Button[]{a1,a2,a3,b1,b2,b3,c1,c2,c3};
         
         for(Button b:bArray){
         	b.setOnClickListener(this);
         }
-           
+       NewGame.setOnClickListener(new OnClickListener(){
+    	   
+    	   public void onClick(View v){
+    	    	//Button b= (Button) v;
+    	    	//buttonClicked(b);
+    	    	turn =true;
+    	    	turn_count=0;
+    	    	enabledisableAllButton(true);
+    	    }   
+    	   
+       });
     }
 
 
@@ -54,28 +66,28 @@ public class MainActivity extends Activity implements OnClickListener {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    public void onClick(View v){
-    	Button b= (Button) v;
-    	buttonClicked(b);
+   
+    public void onClick(View v) {
+    	// TODO Auto-generated method stub
+    	buttonClicked(v);
     }
 
-
-public void buttonClicked(Button b){
+private void buttonClicked(View v){
 	
-	
+	Button b= (Button)v;
 	if(turn){
 		
-		b.setText("X");
+		((TextView) v).setText("X");
 		
 	}else{
 		
-		b.setText("O");
+		((TextView) v).setText("0");
 	}
 	
 	turn_count++;
 	
-	b.setBackgroundColor(Color.LTGRAY);
-	b.setClickable(false);
+	v.setBackgroundColor(Color.LTGRAY);
+	v.setClickable(false);
 	turn =!turn;
 	checkForWinner();
 	
@@ -115,44 +127,47 @@ private void checkForWinner() {
 	else if(a3.getText()==b2.getText()&& b2.getText()==c1.getText()
 			&& !b2.isClickable())
 		there_is_a_winner=true;
-	
-	
-	
-	if(there_is_a_winner){
+			
+	if(there_is_a_winner)
+	{
 		if(!turn){
-			messaje("X Gana");
+			toast("X gana");
 		}else{
-			message("O Gana");
-		}	
-		enabledisableAllButtons(false);
+			toast("O gana");
+		}
+		enabledisableAllButton(false);
+	}else if(turn_count ==9){
+		toast("DWAW");
 	}
-	
-   }
-
-
-private void enabledisableAllButtons(boolean enable) {
-	// TODO Auto-generated method stub
-	for(Button b :bArray){
-		b.setClickable(enable);
-		if(enable)
-			b.setBackgroundColor(Color.parseColor("33b5e5"));
-	}
-	
 }
 
 
+
+private void enabledisableAllButton(boolean enable){
+	
+	for(Button b:bArray){
+		b.setClickable(enable);
+		
+		if(enable){
+			
+			b.setBackgroundColor(Color.parseColor("#33b5e5"));
+			b.setText("");
+		}
+		else{
+			b.setBackgroundColor(Color.LTGRAY);
+		}
+	}
+	
+}
 private void toast(String message) {
 	// TODO Auto-generated method stub
-	Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-}
+	Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
 	
+		
 }
 
 
 
 
 
-
-
-
-
+}
